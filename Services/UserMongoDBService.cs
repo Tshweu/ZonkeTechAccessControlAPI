1b8215976db3a6a1a0d459383f57b44d83e9ddde
Tshweu Sephiri <t.t.sephiri@gmail.com>
2023-05-29 13:39:07 +0200

    User mongodb service started

---
+++ Services/UserMongoDBService.cs
@@ -0,0 +1,12 @@
+namespace ZonkeTechAccessControlAPI.Services;
+
+using MongoDB.Driver;
+using ZonkeTechAccessControlAPI.Models;
+
+public class UserMongoDBService{
+    private readonly IMongoCollection<User> _userCollection;
+
+
+
+    
+}
