487ad9700b7df43da8809ce73f2b25709d3bfa9d
Tshweu Sephiri <t.t.sephiri@gmail.com>
2023-05-29 12:22:26 +0200

    added user model without mutator validation

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+namespace ZonkeTechAccessControlAPI.Models;
+
+
+public class User{
+    public string Name{get;set;}
+    public string Surname{get;set;}
+    public string PhoneNumber{get;set;}
+    public string Email{get;set;}
+    public string Password{get;set;}
+    public DateTime LastLogin{get;set;}
+
+}
