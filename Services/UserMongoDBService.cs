dee3df329b8ce977a7f7674757d9cc858eb592e5
Tshweu Sephiri <t.t.sephiri@gmail.com>
2023-05-30 16:46:53 +0200

    added get to user service

--- Services/UserMongoDBService.cs
+++ Services/UserMongoDBService.cs
@@ -8,8 +8,17 @@ public class UserMongoDBService{
     private readonly IMongoCollection<User> _userCollection;
 
     public UserMongoDBService(IMongoClient mongoClient){
-        
+        var database = mongoClient.GetDatabase("ZonkeTechDemo");
+        _userCollection = database.GetCollection<User>("Users");
     }
-
+    public async Task<List<User>> GetAsync() {
+        return await _userCollection.Find(_ => true).ToListAsync();
+    }
+    public async Task<User?> GetAsync(string id){
+        return await _userCollection.Find(user => user.Id == id).FirstOrDefaultAsync();
+    }
     
+    public async Task UpdateAsync(string id,User updatedBook) {
+        await _userCollection.ReplaceOneAsync(x => x.Id == id,updatedBook);
+    }
 }
