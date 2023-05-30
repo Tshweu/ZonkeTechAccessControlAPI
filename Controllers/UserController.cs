faf1cfabec279aeba05c57d6df4b833537d22e57
Tshweu Sephiri <t.t.sephiri@gmail.com>
2023-05-30 17:15:41 +0200

    User controller update

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ZonkeTechAccessControlAPI.Models;
-
+using ZonkeTechAccessControlAPI.Services;
 namespace ZonkeTechAccessControlAPI.Controllers;
 
 [ApiController]
@@ -8,19 +8,47 @@ namespace ZonkeTechAccessControlAPI.Controllers;
 public class UserController : ControllerBase
 {
     private readonly ILogger<UserController> _logger;
-
-    public UserController(ILogger<UserController> logger)
+    private readonly UserMongoDBService _userService;
+    
+    public UserController(ILogger<UserController> logger,UserMongoDBService userService)
     {
         _logger = logger;
+        _userService = userService;
     }
 
-    [HttpGet(Name = "GetUser")]
-    public IEnumerable<User> Get()
+    [HttpGet()]
+    public Task<List<User>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new User
-        {
-            LastLogin = DateTime.Now
-        })
-        .ToArray();
+        return _userService.GetAsync();
+    }
+
+    [HttpGet("{id:length(24)}")]
+    public Task<User?> Get(string id)
+    {
+        return _userService.GetAsync(id);
+    }
+    
+    [HttpPost("/create")]
+    public async Task<IActionResult> Post(User newUser){
+        await _userService.CreateAsync(newUser);
+        return CreatedAtAction(nameof(Get),new {id = newUser.Id },newUser);
+    }
+
+    [HttpPut("/update/{id:length(24)}")]
+    public async Task<IActionResult> Put(string id,User updatedUser){
+        var user = await _userService.GetAsync(id);
+        if(user is null){
+            return NotFound();
+        }
+        
+        updatedUser.Id = id;
+        await _userService.UpdateAsync(id,updatedUser);
+        return NoContent();
+    }
+
+    [HttpPut("/delete/{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id){
+        await _userService.DeleteAsync(id);
+        return null;
     }
 }
